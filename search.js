fcf85ffff386118ea14b51a03e390f6c9cf4cf9e
email@jasoncostello.com <email@jasoncostello.com>
2022-06-26 12:27:27 -0500

    initial

---
+++ search.js
@@ -0,0 +1,6 @@
+function searchReddit() {
+    var query = document.getElementById("inputText").value;
+   var url =  "https://www.google.com/search?q="+query+"+site%3Areddit.com&oq="+query+"+site%3Areddit.com"
+    console.log("url: ", url);
+    window.location.href = url;
+}
