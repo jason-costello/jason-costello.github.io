7b0062d7f20e9e0aa0d0374ea08ce5ffce009024
email@jasoncostello.com <email@jasoncostello.com>
2022-06-26 17:55:51 -0500

    add evenet listener

--- search.js
+++ search.js
@@ -1,15 +1,8 @@
-function searchReddit() {
-    var query = document.getElementById("inputText").value;
-   var url =  "https://www.google.com/search?q="+query+"+site%3Areddit.com&oq="+query+"+site%3Areddit.com"
-    console.log("url: ", url);
-    window.location.href = url;
-}
 
-// Get the input field
-var input = document.getElementById("inputText");
-input.addEventListener("keypress", function(event) {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    document.getElementById("search").click();
-  }
-});
+
+function searchReddit() {
+  var query = document.getElementById("inputText").value;
+  var url =  "https://www.google.com/search?q="+query+"+site%3Areddit.com&oq="+query+"+site%3Areddit.com"
+  console.log("url: ", url);
+  window.location.href = url;
+}
